45d40779976ec639657dda4c90191731623ca020
Stefan Wong <stfnwong@gmail.com>
2021-08-14 18:32:50 +0100

    Add installer for vim-plug

---
+++ vim_plug_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Just a script for the vim-plug installation
+
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
