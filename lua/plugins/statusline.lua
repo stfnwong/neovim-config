7a9fae69131b3e8b26cc8d7f494af2a6ab6ab0df
Stefan Wong <stfnwong@gmail.com>
2026-05-07 21:28:31 +0100

    Change filename display

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -10,7 +10,7 @@ return {
 		sections = {
 			lualine_a = { "mode" },
 			lualine_b = { "branch", "diff", "diagnostics" },
-			lualine_c = { "filename" },
+			lualine_c = { { "filename", path = 2 } },
 			lualine_x = { "filetype" },
 			lualine_y = { "progress" },
 			lualine_z = { "location" },
