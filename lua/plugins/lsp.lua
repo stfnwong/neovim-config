795beee47b620218279bdb6e0e3dc6a4e873c3f5
Stefan Wong <stfnwong@gmail.com>
2026-05-13 01:31:16 +0100

    Tried to shut notifications up but failed

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -14,6 +14,7 @@ return {
 		},
 		opts = {
 			ensure_installed = {
+				"basedpyright",
 				"lua_ls",
 				"clangd",
 				"rust_analyzer",
@@ -95,6 +96,17 @@ return {
 								functionReturnTypes = false,
 								genericTypes = false,
 							},
+							-- lua handlers = { --- filter noisy notifications ['$/progress'] = function(err, result, ctx) -- just notify once if result.token == (vim.g.basedpyright_progress_token or result.token) then vim.g.basedpyright_progress_token = result.token vim.lsp.handlers['$/progress'](err, result, ctx) end end, },
+
+							-- Try to shut these infernal notifications up
+							--lua_handlers = {
+							--	["$/progress"] = function(err, result, ctx)
+							--		if result.token == (vim.g.basedpyright_progress_token or result.token) then
+							--			vim.g.basedpyright_progress_token =
+							--				result.token.vim.lsp.handlers["$/progress"](err, result, ctx)
+							--		end
+							--	end,
+							--},
 						},
 					},
 				},
