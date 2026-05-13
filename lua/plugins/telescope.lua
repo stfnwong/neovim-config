6d88c732d194e74f4c481f076df6f80cfd7ff8ea
Stefan Wong <stfnwong@gmail.com>
2026-05-13 01:49:27 +0100

    More telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -19,6 +19,22 @@ return {
 						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
 					},
 				},
+				pickers = {
+					lsp_references = {
+						show_line = false, -- don't show the code line in results, less noise
+						include_declaration = false, -- don't include the definition itself in references
+						fname_width = 40,
+					},
+					lsp_definitions = {
+						show_line = false,
+					},
+					lsp_implementations = {
+						show_line = false,
+					},
+					lsp_document_symbols = {
+						symbol_width = 40,
+					},
+				},
 			},
 		})
 
