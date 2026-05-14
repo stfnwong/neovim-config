5b8a706bea47357d6acbf7463b4e38a05f941649
Stefan Wong <stfnwong@gmail.com>
2026-05-14 07:33:13 +0100

    Add command to dump current keymap

--- init.lua
+++ init.lua
@@ -12,3 +12,12 @@ require("core.lsp_attach")
 -- Try turning netrw back on
 -- vim.g.loaded_netrw = 0
 -- vim.g.loaded_netrwPlugin = 0
+
+-- Add command to dump current keymaps
+vim.api.nvim_create_user_command("DumpKeymaps", function()
+	local output = vim.api.nvim_exec2("nnoremap", { output = true }).output
+	local lines = vim.split(output, "\n")
+	local path = vim.fn.expand("~/nvim_keymaps.txt")
+	vim.fn.writefile(lines, path)
+	vim.notify("Keymaps written to " .. path)
+end, {})
